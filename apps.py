77cf019905c0652f7f18113e423900c005da0439
Violakataike <67233047+Violakataike@users.noreply.github.com>
2020-09-17 16:46:01 +0300

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,18 @@
+class BankAccount():
+    name="Viola Kataike"
+    balance=0.0
+    def __init__(self, name, balance):
+        self.name=Viola Kataike
+self.balance=0.0
+print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+def deposit(self):
+amount=float(input("Enter amount to be Deposited: "))
+self.balance += amount
+print("\n Amount Deposited:",amount)
+def withdraw(self):
+amount = float(input("Enter amount to be Withdrawn: "))
+
+else:
+    print("\n Insufficient balance ")
+def display(self):
+print("\n Net Available Balance=",self.balance)
